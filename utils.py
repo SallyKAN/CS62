97d586fc86c8e7f0a677910974548b13606c2eaf
SallyKAN <491251735@qq.com>
2020-04-27 21:15:22 +0800

    add load data function in utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-
 def create_dataset(out_dir):
     # Load training data
     data_path = os.path.join(out_dir, "data.pkl")
@@ -38,6 +37,30 @@ def create_dataset(out_dir):
     y_test_outfile = open(os.path.join(out_dir, "y_test.pkl"), 'wb')
     pickle.dump(y_test, y_test_outfile)
 
+def load_data_google_home():
+    print("Loading Google_Home dataset for training")
+    dataset_dir = '/home/snape/Documents/comp5703/pickle_data/Google_Home/Captures_5m/'
+
+    # Load training data
+    with open(dataset_dir + 'X_training.pkl', 'rb') as handle:
+        X_train = np.array(pickle.load(handle))
+    with open(dataset_dir + 'y_training.pkl', 'rb') as handle:
+        y_train = np.array(pickle.load(handle))
+
+    # Load testing data
+    with open(dataset_dir + 'X_test.pkl', 'rb') as handle:
+        X_test = np.array(pickle.load(handle))
+    with open(dataset_dir + 'y_test.pkl', 'rb') as handle:
+        y_test = np.array(pickle.load(handle))
+
+    print("Data dimensions:")
+    print("X: Training data's shape : ", X_train.shape)
+    print("y: Training data's shape : ", y_train.shape)
+
+    print("X: Testing data's shape : ", X_test.shape)
+    print("y: Testing data's shape : ", y_test.shape)
+
+    return X_train, y_train, X_test, y_test
 
 if __name__ == '__main__':
     aamazon5_data_dir = "/home/snape/Documents/comp5703/pickle_data/Amazon_Echo/Captures_5m"
